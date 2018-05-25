a4b1e8636194ecafb8c61fe0e207d9eda2fb984b
Reynier de la Rosa <reynier.delarosa@digimobil.es>
2018-05-25 12:38:58 +0200

    add tests for episodes and tv shows

--- test/Feature/EpisodeHomeTest.php
+++ test/Feature/EpisodeHomeTest.php
@@ -14,28 +14,37 @@ use PHPUnit\Framework\TestCase;
 class EpisodeHomeTest extends TestCase
 {
 
+    /** @var Home $imdbScrapper */
+    protected $imdbScrapper;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->imdbScrapper = (new Home())->setImdbNumber(5655178)->setContentFromUrl();
+        parent::__construct($name, $data, $dataName);
+    }
+
     public function testGetTitle()
     {
-
+        $this->assertEquals('Stormborn', $this->imdbScrapper->getTitle());
     }
 
     public function testGetEpisodeNumber()
     {
-
+        $this->assertEquals(2, $this->imdbScrapper->getEpisodeNumber());
     }
 
     public function testIsEpisode()
     {
-
+        $this->assertEquals(true, $this->imdbScrapper->isEpisode());
     }
 
     public function testGetSeasonNumber()
     {
-
+        $this->assertEquals(7, $this->imdbScrapper->getSeasonNumber());
     }
 
     public function testGetTvShow()
     {
-
+        $this->assertEquals(944947, $this->imdbScrapper->getTvShow());
     }
 }
--- test/Feature/TvShowHomeTest.php
+++ test/Feature/TvShowHomeTest.php
@@ -14,13 +14,22 @@ use PHPUnit\Framework\TestCase;
 class TvShowHomeTest extends TestCase
 {
 
+    /** @var Home $imdbScrapper */
+    protected $imdbScrapper;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->imdbScrapper = (new Home())->setImdbNumber(944947)->setContentFromUrl();
+        parent::__construct($name, $data, $dataName);
+    }
+
     public function testGetTitle()
     {
-
+        $this->assertEquals('Game of Thrones', $this->imdbScrapper->getTitle());
     }
 
     public function testIsTvShow()
     {
-
+        $this->assertEquals(true, $this->imdbScrapper->isTvShow());
     }
 }
